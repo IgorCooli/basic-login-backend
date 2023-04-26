a8e1a6a3fb8bc9549581d56403f483232d813e99
Igor <igorcooli@gmail.com>
2023-04-26 19:33:38 -0300

    initial commit

---
+++ src/main/java/br/com/basicprojectbackend/BasicProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.basicprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/basicprojectbackend/BasicProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.basicprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
